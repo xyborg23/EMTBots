da6f52113dc60d28a3baabd8faf26748bf7c5f98
Rakshit Choudhary <rgngrpch@users.noreply.github.com>
2016-10-26 14:57:17 -0500

    Add files via upload
    Lcc edited

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var http = require('http');
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 var options = {
   host: 'dsspp.skoonline.org',
-  path: '/base?json={"ttop":50,"category":"news","include_etop":true,"SS":"fa","wc":0,"format":"xml","minWeight":0,"userGuid":"0ba98b2e-6da0-4096-b993-954a5790e726","type":"2","text":"happy","minStrength":0,"guid":"0ba98b2e-6da0-4096-b993-954a5790e726","include_ttop":true,"minRankby":0,"etop":10,"domain":"nodomain"}'
+  path: '/lcc?json={"ttop":50,"category":"news","include_etop":true,"target":"","SS":"fa","wc":0,"notes":"","sessionKey":"ag9zfmRzc3BwMjAxMS1ocmRyFwsSCkxDQ1Nlc3Npb24YgICAoK2_mAoM","format":"xml","minWeight":0,"userGuid":"44064767-a6ef-4c70-9536-cf196ee6794a","type":"2","text":"","minStrength":0,"current":"bad%20disease","guid":"ea8308d1-f93c-457d-84c8-1fa4457c7148","include_ttop":true,"minRankby":0,"etop":10,"domain":"nodomain"}'
 };
 
 callback = function(response) {
